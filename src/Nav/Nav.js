8c4e2c173cbc70fe3c57ec4b4188d4dc72e6f97f
Dan McQuade <dtmcquade@gmail.com>
2017-12-18 15:29:54 -0500

    Add styles for navbar

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import './Nav.css'
 
 class Nav extends Component {
   render () {
     return (
       <nav>
-        <ul>
-          <li> <Link to='/dogs'>Dogs</Link></li>
-          <li> <Link to='/about'>About</Link></li>
-          <li> <Link to='/resources'>Resources</Link></li>
-          <li> <Link to='/contact'>Contact</Link></li>
-        </ul>
+        <Link to='/dogs'>Dogs</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/resources'>Resources</Link>
+        <Link to='/contact'>Contact</Link>
       </nav>
     )
   }
