e52f9d4df23ab09492c4c6e4e82e0234c62e0eb3
Dan McQuade <dtmcquade@gmail.com>
2017-12-18 14:34:47 -0500

    Add router functionality to main App component

--- src/About/About.js
+++ src/About/About.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react'
-import Nav from '../Nav/Nav.js'
-import Footer from '../Footer/Footer.js'
 
 class About extends Component {
   render () {
     return (
-      <div>
-        <Nav />
-        <div class="content">
+      <div class='content'>
         <h1>iPaws</h1>
+
         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi.</p>
-        </div>
-        <Footer />
       </div>
     )
   }
--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
-import logo from '../logo.svg'
+import {
+  Link,
+  Redirect,
+  Route,
+  Switch
+} from 'react-router-dom'
 import './App.css'
+import About from '../About/About.js'
 import Main from '../Main/Main.js'
 import Nav from '../Nav/Nav.js'
 import Footer from '../Footer/Footer.js'
@@ -9,15 +15,23 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Nav />
-        <Main />
+        <main>
+          <Switch>
+            <Route exact path='/' render={() => (
+              <Main />
+            )} />
+            <Route exact path='/About' render={() => (
+              <About />
+            )} />
+            <Route
+              path='/*'
+              render={() => (
+                <Redirect to='/' />
+              )}
+            />
+          </Switch>
+        </main>
         <Footer />
       </div>
     )
