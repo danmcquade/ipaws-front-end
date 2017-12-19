864cbdcc135ac6517df1968ed7bbeee3e3e00006
Dan McQuade <dtmcquade@gmail.com>
2017-12-19 09:44:15 -0500

    Add Inquire form (2nd model) to ShowDog component

--- src/ShowDog/ShowDog.js
+++ src/ShowDog/ShowDog.js
@@ -9,8 +9,62 @@ class ShowDog extends Component {
     this.state = {
       dog_id: this.props.match.params.id,
       inquiries: [],
-      dog: {}
+      dog: {},
+      inquiry_name: '',
+      inquiry_email: '',
+      inquiry_phone: '',
+      inquiry_location: '',
+      inquiry_comment: ''
     }
+  }
+
+  handleSubmit (e) {
+    e.preventDefault()
+    axios.post(`http://localhost:3001/api/dogs/newinquiry/${this.state.dog_id}`, {
+      name: this.state.inquiry_name,
+      email: this.state.inquiry_email,
+      phone: this.state.inquiry_phone,
+      location: this.state.inquiry_location,
+      comment: this.state.inquiry_comment
+    })
+    .then((response) => {
+      console.log(response)
+      window.location.reload()
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
+  handleNameInput (e) {
+    this.setState({
+      inquiry_name: e.target.value
+    })
+    console.log(this.state.inquiry_name)
+  }
+  handleEmailInput (e) {
+    this.setState({
+      inquiry_email: e.target.value
+    })
+    console.log(this.state.inquiry_email)
+  }
+  handlePhoneInput (e) {
+    this.setState({
+      inquiry_phone: e.target.value
+    })
+    console.log(this.state.inquiry_phone)
+  }
+  handleLocationInput (e) {
+    this.setState({
+      inquiry_location: e.target.value
+    })
+    console.log(this.state.inquiry_location)
+  }
+  handleCommentInput (e) {
+    this.setState({
+      inquiry_comment: e.target.value
+    })
+    console.log(this.state.inquiry_comment)
   }
 
   componentWillMount () {
@@ -58,6 +112,32 @@ class ShowDog extends Component {
               <p><strong>Description:</strong></p><p>{this.state.dog.description}</p>
               <p><Link to={editLink}>Edit {this.state.dog.name}&apos;s Details</Link></p>
             </div>
+          </div>
+          <div className='inquire-form'>
+            <h3>Inquire About {this.state.dog.name}</h3>
+            <form onSubmit={(e) => this.handleSubmit(e)}>
+              <table>
+                <tr>
+                  <td><p><label>Your name</label></p>
+                    <p><input type='text' onChange={(e) => this.handleNameInput(e)} placeholder='Name' /></p>
+                  </td>
+                  <td><p><label>Email address</label></p>
+                    <p><input type='text' onChange={(e) => this.handleEmailInput(e)} placeholder='E-mail' /></p>
+                  </td>
+                </tr>
+                <tr>
+                  <td><p><label>Phone number</label></p>
+                    <p><input type='text' onChange={(e) => this.handlePhoneInput(e)} placeholder='Phone' /></p></td>
+                  <td><p><label>Location</label></p>
+                    <p><input type='text' onChange={(e) => this.handleLocationInput(e)} placeholder='Location' /></p></td>
+                </tr>
+                <tr>
+                  <td><p><label>Comment</label></p>
+                    <p><input type='text' onChange={(e) => this.handleCommentInput(e)} placeholder='Comment' /></p></td>
+                  <td><button type='submit'>Inquire</button></td>
+                </tr>
+              </table>
+            </form>
           </div>
         </div>
       </div>
