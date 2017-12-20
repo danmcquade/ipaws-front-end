9aaa0c0b24af4993fc55f066b5cb236f6a81985a
ANTE BOZIC <antebozic@gmail.com>
2017-12-20 10:21:51 -0500

    updated comment form

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,45 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
+import "./Contact.css"
 
 class Contact extends Component {
+	render() {
+		return (
+			<div class="content">
+				<h1>Contact iPaws</h1>
 
+				<div className="contact-form">
+					<form>
+						<label> Name </label>
+						<p>
+							<input type="text" />
+						</p>
+						<label>Email</label>
+						<p>
+							<input type="text" />
+						</p>
+						<label className="telephone">Telephone</label>
+						<p>
+							<input type="text" />
+						</p>
 
+						<label>Address/Location</label>
+						<p>
+							<input type="text" />
+						</p>
 
+						<label>Message</label>
+						<p>
+							<input type="text" />
+						</p>
 
-
-  render () {
-    return (
-      <div class='content'>
-        <h1>Contact iPaws</h1>
-
-      <div className="contact-form">
-      <form>
-        <label><li> Name </li></label>
-          <p>
-            <input type="text" />
-              </p>
-              <label><li> Email </li></label>
-              <p>
-                <input type="text" />
-                </p>
-                <label className='telephone'><li> Telephone # </li></label>
-                <p>
-                  <input type="text" />
-                    </p>
-                    <label><li>Message</li></label>
-                    <p>
-                      <input type="text" />
-                      </p>
-                      <label><li> Address/Location </li></label>
-                      <p>
-                        <input type="text" />
-                          </p>
-                           <input type="submit" />
-
-              </form>
-            </div>
-          </div>
-    )
-  }
+						<input type="submit" />
+					</form>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default Contact
