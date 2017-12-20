dfe4bb9b1d738325cddff364dc488c3734253d7b
debutler266 <32465329+debutler266@users.noreply.github.com>
2017-12-20 16:36:06 -0500

    re-fixed contact page to now direct to ContactSubmission Page when users submit the form

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,61 +1,58 @@
 import React, { Component } from "react"
 import "./Contact.css"
+import { Redirect } from 'react-router'
 
 class Contact extends Component {
-	render() {
+	constructor () {
+		super();
+		this.state = {
+			fireRedirect: null
+		}
+	}
+	submitForm = (e) => {
+		e.preventDefault()
+		this.setState({ fireRedirect: true })
+	}
+	render () {
+		const fireRedirect = this.state.fireRedirect
+
 		return (
-			<div className="content">
-				<img class='main-logo' src='/logo.png' alt='iPaws Logo' />
+			<div class="content">
 				<h1>Contact iPaws</h1>
           <p>iPaws does not run a dedicated facility to house homeless pets. For this reason, we ask that if you would like to meet a particular pet, please fill out the contact form below with your Name, Telephone number, current Address, Email, and a brief Message just stating your interest in a particular dog(s).
             However, although we strongly recommend filling out the contact form if you have interest, you are also welcome to simply come out to an adoption event! </p>
 
 				<div className="contact-form">
-					<form>
-					<table>
-						<tbody>
-						<tr>
-						<td>
+					<form onSubmit={this.submitForm}>
+
 						<label> Name </label>
 						<p>
 							<input type="text" />
 						</p>
-						</td>
-						<td>
 						<label>Email</label>
 						<p>
 							<input type="text" />
 						</p>
-						</td>
-						</tr>
-						<tr>
-						<td>
 						<label className="telephone">Telephone</label>
 						<p>
 							<input type="text" />
 						</p>
-						</td>
-						<td>
+
 						<label>Address/Location</label>
 						<p>
 							<input type="text" />
 						</p>
-						</td>
-						</tr>
-						<tr>
-						<td>
+
 						<label>Message</label>
 						<p>
-						<textarea
-							type="text"
-						/>
+							<input type="text" />
 						</p>
-						</td>
-						</tr>
+
 						<input type="submit" />
-							</tbody>
-						</table>
 					</form>
+					{fireRedirect && (
+						<Redirect to= '/contact-submit' />
+						)}
 				</div>
 			</div>
 		)
