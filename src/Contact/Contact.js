a2c5a88cbea26f0c6b2d9d68ca4e3763d027700e
ANTE BOZIC <antebozic@gmail.com>
2017-12-20 16:01:15 -0500

    updated contact style

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -4,37 +4,57 @@ import "./Contact.css"
 class Contact extends Component {
 	render() {
 		return (
-			<div class="content">
+			<div className="content">
+				<img class='main-logo' src='/logo.png' alt='iPaws Logo' />
 				<h1>Contact iPaws</h1>
           <p>iPaws does not run a dedicated facility to house homeless pets. For this reason, we ask that if you would like to meet a particular pet, please fill out the contact form below with your Name, Telephone number, current Address, Email, and a brief Message just stating your interest in a particular dog(s).
             However, although we strongly recommend filling out the contact form if you have interest, you are also welcome to simply come out to an adoption event! </p>
 
 				<div className="contact-form">
 					<form>
+					<table>
+						<tbody>
+						<tr>
+						<td>
 						<label> Name </label>
 						<p>
 							<input type="text" />
 						</p>
+						</td>
+						<td>
 						<label>Email</label>
 						<p>
 							<input type="text" />
 						</p>
+						</td>
+						</tr>
+						<tr>
+						<td>
 						<label className="telephone">Telephone</label>
 						<p>
 							<input type="text" />
 						</p>
-
+						</td>
+						<td>
 						<label>Address/Location</label>
 						<p>
 							<input type="text" />
 						</p>
-
+						</td>
+						</tr>
+						<tr>
+						<td>
 						<label>Message</label>
 						<p>
-							<input type="text" />
+						<textarea
+							type="text"
+						/>
 						</p>
-
+						</td>
+						</tr>
 						<input type="submit" />
+							</tbody>
+						</table>
 					</form>
 				</div>
 			</div>
