f0e16f0282c3e9b578f90d790004adfbb0f4eb83
Dan McQuade <dtmcquade@gmail.com>
2017-12-20 15:23:59 -0500

    Add route for contact form submission

--- src/ContactSubmission/ContactSubmission.js
+++ src/ContactSubmission/ContactSubmission.js
@@ -4,10 +4,9 @@ class ContactSubmission extends Component {
   render () {
     return (
       <div>
-      <img class='main-logo' src='/logo.png' alt='iPaws Logo' />
-      <h1>Woof, Woof!, Thank You!</h1>
-      <p>We would like to personally thank you for your interest in one of our dogs. It means the world to us & we will get in touch with you ASAP.
-      </p>
+        <img class='main-logo' src='/logo.png' alt='iPaws Logo' />
+        <h1>Woof, Woof!, Thank You!</h1>
+        <p>We would like to personally thank you for your interest in one of our dogs. It means the world to us & we will get in touch with you ASAP.</p>
       </div>
 
     )
