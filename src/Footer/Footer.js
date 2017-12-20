bb27e48bfcf5b330fdc9fae1b03d5d17a8e89b53
Dan McQuade <dtmcquade@gmail.com>
2017-12-19 23:38:11 -0500

    Merge Ante's footer code

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import './Footer.css'
 
 class Footer extends Component {
   render () {
     return (
-      <div>
-        <h1>Some footer content</h1>
-      </div>
+      <footer>
+        <Link to='/' className='push-left'>
+          iPwas
+        </Link>
+        <a href='#'>
+          <img src='footer/email.png' />
+        </a>
+        <a href='#'>
+          <img src='footer/fb.png' />
+        </a>
+        <a href='#'>
+          <img src='footer/twitter.png' />
+        </a>
+      </footer>
     )
   }
 }
