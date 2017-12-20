4fb87b35fc14348e8b39e8f13f47dbbc3b9f86c3
debutler266 <32465329+debutler266@users.noreply.github.com>
2017-12-20 11:40:20 -0500

    added the intro paragraph to Contact page

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -6,6 +6,8 @@ class Contact extends Component {
 		return (
 			<div class="content">
 				<h1>Contact iPaws</h1>
+          <p>iPaws does not run a dedicated facility to house homeless pets. For this reason, we ask that if you would like to meet a particular pet, please fill out the contact form below with your Name, Telephone number, current Address, Email, and a brief Message just stating your interest in a particular dog(s).
+            However, although we strongly recommend filling out the contact form if you have interest, you are also welcome to simply come out to an adoption event! </p>
 
 				<div className="contact-form">
 					<form>
