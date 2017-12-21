ae4694b11a4e5a9f28de94645660de8e68dd1ffc
Dan McQuade <dtmcquade@gmail.com>
2017-12-21 09:30:32 -0500

    Update contact submission component type

--- src/ContactSubmission/ContactSubmission.js
+++ src/ContactSubmission/ContactSubmission.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './ContactSubmission.css'
 
-class ContactSubmission extends Component {
-  render () {
-    return (
-      <div>
-        <img className='main-logo' src='/logo.png' alt='iPaws Logo' />
-        <div class='contact-submit-content'>
-          <h1>Woof, Woof! Thank You!</h1>
-          <p>Thank you very much for reaching out to iPaws. We strive to respond to all inquiries within 24 hours. A member of our staff will be in touch with you shortly. If you need immediate assistance, please call our office at (202) 728-0010. Thank you!</p>
-        </div>
+const ContactSubmission = () => {
+  return (
+    <div>
+      <img className='main-logo' src='/logo.png' alt='iPaws Logo' />
+      <div class='contact-submit-content'>
+        <h1>Woof, Woof! Thank You!</h1>
+        <p>Thank you very much for reaching out to iPaws. We strive to respond to all inquiries within 24 hours. A member of our staff will be in touch with you shortly. If you need immediate assistance, please call our office at (202) 728-0010. Thank you!</p>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default ContactSubmission
